42a5e83886e130073d88d582a2b154aa36aabf21
Gábor Kiszli <gaborfk@live.com>
2020-10-20 06:44:35 -0700

    10.20-06:45

---
+++ C++/Problem18.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int BinarySearchTree();
+
+
+int main()
+{
+
+    std::cout << std::endl << "Maximum Total: " << BinarySearchTree() << std::endl;
+
+    return 0;
+}
+
+int BinarySearchTree()
+{
+    int intMaximumTotal = 0;
+
+
+
+    return intMaximumTotal;
+}
